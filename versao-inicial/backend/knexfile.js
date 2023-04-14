3280a74f2c82484d44755671e64a64dbb5c7aa2b
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-04-14 19:09:11 -0300

    NodeJS API projeto knowledge crud categorias

---
+++ versao-inicial/backend/knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+    client: 'postgresql',
+    connection: {
+      host : '172.20.0.2',
+      port: 5432,
+      database: 'knowledge',
+      user:     'root',
+      password: 'root'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+};
